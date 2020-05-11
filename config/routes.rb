9cb8299e93f1c7462502eefea3c44f537a0f12f7
Tom <tchulick48@gmail.com>
2020-05-11 16:39:14 -0500

    Created User show action, route, and show json

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   namespace :api do
     get "/users" => "users#index"
     post "/users" => "users#create"
+    get "/users/:id" => "users#show"
 
     post "/sessions" => "sessions#create"
 
